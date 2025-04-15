b46f4ebe88bf381b218733863dc03e05150a426c
Teighin Nordholt <teighinnordholt@gmail.com>
2025-04-15 17:42:37 -0400

    Arduino script for testing

---
+++ TC_Amp/Software/TC_read/TC_read.ino
@@ -0,0 +1,33 @@
+//analog pin to read
+#define TC_PIN A0
+
+//amplifier constants
+float bias = 0.4;
+float gain = 1.5;
+
+//linear seeback coefficient
+float seeback_coeff = 40e-6;
+
+float volt = 0;
+float temp = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  //read analog pin
+  volt = analogRead(TC_PIN) * (5.0 / 1023.0);
+  
+  //convert to temperature
+  temp = (volt - bias) / (gain * seeback_coeff);
+
+  Serial.print("Raw Voltage: ");
+  Serial.println(volt, 5);
+
+  Serial.print("Temperature (C): ");
+  Serial.println(temp, 4);
+  Serial.println();
+
+  delay(1000);
+}
